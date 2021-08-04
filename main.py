6112bdd1604141e08f158b02de9891f78db852bf
dai03 <d.taniguchi0803@gmail.com>
2021-08-04 23:27:39 +0900

    templete入力完成

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from check import Check
+from constants import Constants
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import time
@@ -10,29 +10,28 @@ dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROME_DRIVER_PATH"))
-driver.get("https://www.google.com/")
-time.sleep(2)
+# driver.get("https://www.google.com/")
 
 #マネーフォワードログインページ
 driver.get("https://attendance.moneyforward.com/employee_session/new")
+time.sleep(2)
 
-time.sleep(3)
 #会社ID入力
-for i in driver.find_elements_by_xpath("//*[@id='employee_session_form_office_account_name']"):
-     i.send_keys(os.environ.get("CORPORATION_ID"))
+input_corporation_id = driver.find_element_by_xpath("//*[@id='employee_session_form_office_account_name']")
+input_corporation_id.send_keys(os.environ.get("CORPORATION_ID"))
 #メールアドレス入力
-for i in driver.find_elements_by_xpath("//*[@id='employee_session_form_account_name_or_email']"):
-     i.send_keys(os.environ.get("EMAIL_ADDRESS"))
+input_email_address = driver.find_element_by_xpath("//*[@id='employee_session_form_account_name_or_email']")
+input_email_address.send_keys(os.environ.get("EMAIL_ADDRESS"))
 #パスワード入力
-for i in driver.find_elements_by_xpath("//*[@id='employee_session_form_password']"):
-     i.send_keys(os.environ.get("PASSWORD"))
+input_password = driver.find_element_by_xpath("//*[@id='employee_session_form_password']")
+input_password.send_keys(os.environ.get("PASSWORD"))
 #ログイン押下
-for i in driver.find_elements_by_xpath("/html/body/div[1]/div/div/div/div/form/div[4]/input"):
-     i.click()
+click_login = driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/form/div[4]/input")
+click_login.click()
 
 #日時勤怠押下
-for i in driver.find_elements_by_xpath("//*[@id='kt-attendance-header-navigation-item-attendances']/a"):
-     i.click()
+click_attendance = driver.find_element_by_xpath("//*[@id='kt-attendance-header-navigation-item-attendances']/a")
+click_attendance.click()
 
 # time.sleep(3)
 # #使い方ツアー閉じる
@@ -44,36 +43,61 @@ for i in driver.find_elements_by_xpath("//*[@id='kt-attendance-header-navigation
 #      txt = i.find_element_by_class_name("attendance-table-header-month-range-text").text
 #print(txt)
 
-for i in driver.find_elements_by_xpath('/html/body/div[1]/div[2]/div/div/div/div[2]/div[1]/div[2]/a[1]'):
-     i.click()
+#一括編集押下
+click_bulk_editing = driver.find_element_by_xpath('/html/body/div[1]/div[2]/div/div/div/div[2]/div[1]/div[2]/a[1]')
+click_bulk_editing.click()
 
 time.sleep(3)
 
-# for i, g in enumerate(driver.find_elements_by_xpath("/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody")):
-#      print(i)
-
-check = Check()
-cou = 0
 for i, g in enumerate(driver.find_elements_by_class_name("column-day")):
      if(i == 0):
           continue
-     path = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[2]/div".format(i)
-     status = check.check_day_of_week(path, driver)
 
-     if(status):
-          cou = cou + 1
+     path_week_day = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[2]/div".format(i)
 
-print(cou)
+     for j in driver.find_elements_by_xpath(path_week_day):
+          if(j.text == Constants.WEEKDAY):
+               #勤務時間帯を選択
+               path_select = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[3]/select".format(i)
+               select = Select(driver.find_element_by_xpath(path_select))
+               select.select_by_value("45770")
+               #出勤時間を入力
+               psth_2 = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[4]/div/input".format(i)
+               input_going_to_work = driver.find_element_by_xpath(psth_2)
+               input_going_to_work.send_keys("9:00")
+               #退勤時間を入力
+               path_leaving_work = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[5]/div/input".format(i)
+               input_leaving_work = driver.find_element_by_xpath(path_leaving_work)
+               input_leaving_work.send_keys("18:30")
+               #休憩入りの枠追加
+               path_plus_come_in = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[6]/div/a[1]".format(i)
+               click_plus_come_in = driver.find_element_by_xpath(path_plus_come_in)
+               click_plus_come_in.click()
+               #休憩戻りの枠追加
+               path_plus_return = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[7]/div/a[1]".format(i)
+               click_plus_return = driver.find_element_by_xpath(path_plus_return)
+               click_plus_return.click()
+               #休憩入り時間入力（前半）
+               path_before_rest_in = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[6]/div[1]/input".format(i)
+               input_before_rest_in = driver.find_element_by_xpath(path_before_rest_in)
+               input_before_rest_in.send_keys("12:00")
+               #休憩戻り時間入力（前半）
+               path_before_rest_return = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[7]/div[1]/input".format(i)
+               input_before_rest_return = driver.find_element_by_xpath(path_before_rest_return)
+               input_before_rest_return.send_keys("12:45")
+               #休憩入り時間入力（後半）
+               path_after_rest_in = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[6]/div[2]/input".format(i)
+               input_after_rest_in = driver.find_element_by_xpath(path_after_rest_in)
+               input_after_rest_in.send_keys("17:30")
+               #休憩戻り時間入力（後半）
+               path_after_rest_return = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[7]/div[2]/input".format(i)
+               input_after_rest_return = driver.find_element_by_xpath(path_after_rest_return)
+               input_after_rest_return.send_keys("18:00")
 
+#保存ボタン押下
+# path_save = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/div[2]/div/div/div[2]/input[2]"
+# for i in driver.find_elements_by_xpath(path_save):
+#      i.submit()
 
-#/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[1]
-#/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[1]/td[1]
-# /html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[2]/td[3]
-# /html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[3]/td[3]
-
-
-# search_box = driver.find_element_by_name("q")
-# search_box.send_keys('ChromeDriver')
-# search_box.submit()
-time.sleep(2)
+time.sleep(120)
 driver.quit()

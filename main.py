dfd544d8f654cc76d222578e7b805a8745e17842
dai03 <d.taniguchi0803@gmail.com>
2021-08-06 13:10:12 +0900

    定数リファクタリング、休憩枠任意指定追加

--- main.py
+++ main.py
@@ -57,42 +57,46 @@ for i, g in enumerate(driver.find_elements_by_class_name("column-day")):
 
      for j in driver.find_elements_by_xpath(path_week_day):
           if(j.text == Constants.WEEKDAY):
+               plus_come_in_cou = 1
+               going_to_work_time = "9:00"
+               leaving_work_time = "18:30"
+               before_rest_in_time = "12:00"
+               before_rest_return_time = "12:45"
+               after_rest_in_time = "17:30"
+               after_rest_return_time = "18:00"
                #勤務時間帯を選択
                path_select = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[3]/select".format(i)
                select = Select(driver.find_element_by_xpath(path_select))
-               select.select_by_value("45770")
+               select.select_by_value(Constants.WORKING_HOURS[8])
                #出勤時間を入力
                psth_2 = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[4]/div/input".format(i)
                input_going_to_work = driver.find_element_by_xpath(psth_2)
-               input_going_to_work.send_keys("9:00")
+               input_going_to_work.send_keys(going_to_work_time)
                #退勤時間を入力
                path_leaving_work = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[5]/div/input".format(i)
                input_leaving_work = driver.find_element_by_xpath(path_leaving_work)
-               input_leaving_work.send_keys("18:30")
-               #休憩入りの枠追加
-               path_plus_come_in = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[6]/div/a[1]".format(i)
-               click_plus_come_in = driver.find_element_by_xpath(path_plus_come_in)
-               click_plus_come_in.click()
-               #休憩戻りの枠追加
-               path_plus_return = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[7]/div/a[1]".format(i)
-               click_plus_return = driver.find_element_by_xpath(path_plus_return)
-               click_plus_return.click()
-               #休憩入り時間入力（前半）
-               path_before_rest_in = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[6]/div[1]/input".format(i)
-               input_before_rest_in = driver.find_element_by_xpath(path_before_rest_in)
-               input_before_rest_in.send_keys("12:00")
-               #休憩戻り時間入力（前半）
-               path_before_rest_return = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[7]/div[1]/input".format(i)
-               input_before_rest_return = driver.find_element_by_xpath(path_before_rest_return)
-               input_before_rest_return.send_keys("12:45")
-               #休憩入り時間入力（後半）
-               path_after_rest_in = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[6]/div[2]/input".format(i)
-               input_after_rest_in = driver.find_element_by_xpath(path_after_rest_in)
-               input_after_rest_in.send_keys("17:30")
-               #休憩戻り時間入力（後半）
-               path_after_rest_return = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[7]/div[2]/input".format(i)
-               input_after_rest_return = driver.find_element_by_xpath(path_after_rest_return)
-               input_after_rest_return.send_keys("18:00")
+               input_leaving_work.send_keys(leaving_work_time)
+               #指定数分の休憩枠追加
+               if(plus_come_in_cou != None):
+                    for plus in range(plus_come_in_cou):
+                         plus = plus + 1
+                         #休憩入りの枠追加
+                         path_plus_come_in = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[6]/div/a[1]".format(i)
+                         click_plus_come_in = driver.find_element_by_xpath(path_plus_come_in)
+                         click_plus_come_in.click()
+                         #休憩戻りの枠追加
+                         path_plus_return = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[7]/div/a[1]".format(i)
+                         click_plus_return = driver.find_element_by_xpath(path_plus_return)
+                         click_plus_return.click()
+               
+                         #休憩入り時間入力
+                         path_before_rest_in = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[6]/div[{}]/input".format(i, plus)
+                         input_before_rest_in = driver.find_element_by_xpath(path_before_rest_in)
+                         input_before_rest_in.send_keys(before_rest_in_time)
+                         #休憩戻り時間入力
+                         path_before_rest_return = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/table/tbody/tr[{}]/td[7]/div[{}]/input".format(i, plus)
+                         input_before_rest_return = driver.find_element_by_xpath(path_before_rest_return)
+                         input_before_rest_return.send_keys(before_rest_return_time)
 
 #保存ボタン押下
 # path_save = "/html/body/div[1]/div[2]/div/div/div/div[2]/div/form/div[2]/div/div/div[2]/input[2]"
